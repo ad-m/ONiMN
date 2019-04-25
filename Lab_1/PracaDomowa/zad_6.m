9bbef3481ff41c2b73f92a38165fef843a87f87f
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-04-26 00:28:29 +0200

    Update style

--- Lab_1/PracaDomowa/zad_6.m
+++ Lab_1/PracaDomowa/zad_6.m
@@ -6,14 +6,14 @@ A = randn(10:10);
 c=[];
 array=A(:)
 for idx = 1:numel(array)
-    element = array(idx)
+    element = array(idx);
     if element > 0
         c = [c;element];
     end
 end
 disp('3. Czy jakis element macierzy jest mniejszy od 0 (program ma podac 1 jesli tak i 0 jesli nie)')
 any(A(:)<0)
-disp('4. Znajdz wspołrzedne elementu o wartości maksymalnej (numer wiersza i numer kolumny).')
+disp('4. Znajdz wspolrzedne elementu o wartosci maksymalnej (numer wiersza i numer kolumny).')
 [ floor(find(A==max(A(:))) / 10), rem(  find(A==max(A(:))),  10) ]
-disp('5. Wypisz macierz z elementami macierzy A z przedzialu (-0.25,0.25). Pozostale miejsca maja być wypelnione zerami.')
+disp('5. Wypisz macierz z elementami macierzy A z przedzialu (-0.25,0.25). Pozostale miejsca maja byc wypelnione zerami.')
 (abs(A)<0.25) .* A
